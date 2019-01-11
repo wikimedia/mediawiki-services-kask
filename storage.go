bbc1848eb36ea72d2e75308c16651a5cfd31a782
Eric Evans <eevans@wikimedia.org>
2019-01-11 11:06:39 -0600

    Add missing doc strings to `storage.go`

--- storage.go
+++ storage.go
@@ -6,6 +6,9 @@ import (
 	"github.com/gocql/gocql"
 )
 
+// Store is an interface to the underlying data store.
+// Note: For the most part, an interface exists only to enable mocking in tests, not as a means of
+// making storage pluggable.
 type Store interface {
 	Set(string, []byte, int) error
 	Get(string) (Datum, error)
@@ -13,12 +16,14 @@ type Store interface {
 	Close()
 }
 
+// CassandraStore provides access to storage using Apache Cassandra.
 type CassandraStore struct {
 	session  *gocql.Session
 	Keyspace string
 	Table    string
 }
 
+// Datum represents a value returned from storage.
 type Datum struct {
 	Value []byte
 	TTL   int
@@ -32,6 +37,7 @@ func createSession(hostname string, port int, keyspace string) (*gocql.Session, 
 	return cluster.CreateSession()
 }
 
+// NewCassandraStore constructs new instances of CassandraStore.
 func NewCassandraStore(hostname string, port int, keyspace string, table string) (*CassandraStore, error) {
 	session, err := createSession(hostname, port, keyspace)
 	if err == nil {
@@ -40,11 +46,14 @@ func NewCassandraStore(hostname string, port int, keyspace string, table string)
 	return nil, err
 }
 
+// Set stores a new value associated with a key.
+// Values expire after TTL seconds; Values with a TTL of 0 do not expire.
 func (s *CassandraStore) Set(key string, value []byte, ttl int) error {
 	query := fmt.Sprintf(`INSERT INTO "%s"."%s" (key, value) VALUES (?,?) USING TTL ?`, s.Keyspace, s.Table)
 	return s.session.Query(query, key, value, ttl).Consistency(gocql.LocalQuorum).Exec()
 }
 
+// Get retrieves a value associated with a key.
 func (s *CassandraStore) Get(key string) (Datum, error) {
 	var value []byte
 	var ttl int
@@ -53,11 +62,13 @@ func (s *CassandraStore) Get(key string) (Datum, error) {
 	return Datum{value, ttl}, err
 }
 
+// Delete removes a value associated with a key.
 func (s *CassandraStore) Delete(key string) error {
 	query := fmt.Sprintf(`DELETE FROM "%s"."%s" WHERE key = ?`, s.Keyspace, s.Table)
 	return s.session.Query(query, key).Consistency(gocql.EachQuorum).Exec()
 }
 
+// Close terminates the underlying session to Cassandra (disconnects).
 func (s *CassandraStore) Close() {
 	s.session.Close()
 }
